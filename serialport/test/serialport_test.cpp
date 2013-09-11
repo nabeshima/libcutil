5e07cfafec5e3807fd071f31d893a4111869ea7a
cota@upard.org <cota@upard.org@7b271160-a321-6b6a-39b4-b54bf9bb6358>
2013-09-11 05:53:28 +0000

    serialport module is added to support serial communication

---
+++ serialport/test/serialport_test.cpp
@@ -0,0 +1,29 @@
+#include "SerialPort.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace cutil;
+
+int main() {
+  SerialPort sport;
+
+  try {
+    bool ret = sport.open( "/def/ttyACM0", 460800, EIGHT, NONE, ONE );  
+    cout << ret << endl;
+  }
+  catch( Exception &e ) {
+    e.printStackTrace();
+  }
+  
+  while ( sport.isReadable( 20000 ) ) {
+
+    long l = 0;
+    int rc = sport.read( &l, sizeof( l ) );
+    
+    cout << rc << " " << l << endl;
+  }
+  
+
+  return 0;
+}
